efdc9a1e50ab87ae3902ce750da6e8d35f5542da
vlee30 <vlee30@binghamton.edu>
2021-05-04 20:28:43 -0400

    content

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 /* javascript */
 
+/* image slider
+
+let i = 0; // current slide
+let j = 9; // total slides
+
+const dots = document.querySelectorAll(".dot-container button");
+const images = document.querySelectorAll(".image-container img");
+
+function next(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = ( j + i + 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator( i+ 1 );
+}
+
+function prev(){
+    document.getElementById("content" + (i+1)).classList.remove("active");
+    i = (j + i - 1) % j;
+    document.getElementById("content" + (i+1)).classList.add("active");
+    indicator(i+1);
+}
+
+function indicator(num){
+    dots.forEach(function(dot){
+        dot.style.backgroundColor = "transparent";
+    });
+    document.querySelector(".dot-container button:nth-child(" + num + ")").style.backgroundColor = "#8052ec";
+}
+
+function dot(index){
+    images.forEach(function(image){
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
+}
+
+end of image slider */
+
+/* kevin's slider
 $(document).ready(function() {
 
 console.log('hello');
 
+$('#slider-inner').slick({
+    infinite: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
+
 });
+end of kevin's slider*/
